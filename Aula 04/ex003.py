db4a419e10e1a0859d202651d4a7c6c78af703cb
Gabriel da Costa Padilha <44480465+gabrielpadilha-saru@users.noreply.github.com>
2021-01-23 10:43:57 -0300

    Delete Aula 04 directory

--- Aula 04/ex003.py
+++
@@ -1,8 +0,0 @@
-"""
-Crie um programa que leia dois numeros e mostre a soma entre eles.
-"""
-
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-soma = n1 + n2
-print('A soma entre {} + {} vale {}'.format(n1, n2, soma))
