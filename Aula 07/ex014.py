54c8e71b91d98edd72c7cb3a0248cfea625d7a04
Gabriel da Costa Padilha <44480465+gabrielpadilha-saru@users.noreply.github.com>
2021-01-23 10:44:15 -0300

    Delete Aula 07 directory

--- Aula 07/ex014.py
+++
@@ -1,6 +0,0 @@
-"""
-Escreva um programa que converta a temperatura digitada em °C e converta para °F
-"""
-c = float(input('Informe a temperatura em °C: '))
-f = ((9 * c) / 5) + 32
-print(f'A temperatura de {c}°C corresponde a {f}°F')
